62af12f661c26da6d3cadf381b6712d716480f60
sunchengfeng <sunchengfeng@wezonet.com>
2023-03-29 11:30:32 +0800

    fafafa

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
+require('dotenv').config()
 const express = require('express')
-const { createProxyMiddleware } = require('http-proxy-middleware')
-const { Configuration, OpenAIApi } = require('openai')
+// const { Configuration, OpenAIApi } = require('openai')
+const axios = require('axios')
 const app = express()
+app.use(express.json())
 
 // 将请求 /api 转发到 https://example.com/api
 
 app.post('/', async (req, res) => {
-  const configuration = new Configuration({
-    apiKey: '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'
-  })
-  const openai = new OpenAIApi(configuration)
-  const completion = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [{ role: 'system', content: 'xiexiexie ' }]
-  })
-  console.log('req suceess  log')
-  res.send(completion)
+  // const configuration = new Configuration({
+  //   apiKey: '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'
+  // })
+  // const openai = new OpenAIApi(configuration)
+  // const completion = await openai.createChatCompletion({
+  //   model: 'gpt-3.5-turbo',
+  //   messages: [{ role: 'system', content: 'xiexiexie ' }]
+  // })
+  // console.log('req suceess  log')
+  // res.send(completion)
 })
 app.get('/', async (req, res) => {
   try {
-    const configuration = new Configuration({
-      apiKey: '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'
-    })
-    const openai = new OpenAIApi(configuration)
-    const completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'system', content: '前端框架' }]
-    })
-    //ompletion.data.choices[0].message?.content
-    console.log('req suceess log: ', completion.data.choices[0].message.content)
-    res.send(ompletion.data.choices[0].message?.content)
+    await axios.options('https://api.openai.com/v1/chat/completions')
+    await axios
+      .post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+          messages: [{ role: 'system', content: '前端' }],
+          model: 'gpt-3.5-turbo'
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.API_KEY}`
+          }
+        }
+      )
+      .then((data) => {
+        console.log(data)
+      })
   } catch (e) {
-    console.log('fafafafa: ', JSON.stringify(e))
+    console.log('fafafafa: ')
   }
 })
 
 app.listen(3000, () => {
+  console.log(process.env.API_KEY)
   console.log('Server is running on port 3000')
 })
