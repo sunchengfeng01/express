e9b4611490c635573f72616240846664756ffa3c
sunchengfeng <sunchengfeng@wezonet.com>
2023-03-29 18:41:10 +0800

    zt

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 require('dotenv').config()
 const express = require('express')
-// const { Configuration, OpenAIApi } = require('openai')
-const axios = require('axios')
+const { Configuration, OpenAIApi } = require('openai')
+// const axios = require('axios')
 const app = express()
-app.use(express.json())
+const bodyParser = require('body-parser')
+// app.use(express.json())
 
 // 将请求 /api 转发到 https://example.com/api
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY
+})
+const openai = new OpenAIApi(configuration)
+
 app.post('/', async (req, res) => {
-  // const configuration = new Configuration({
-  //   apiKey: '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'
-  // })
-  // const openai = new OpenAIApi(configuration)
-  // const completion = await openai.createChatCompletion({
-  //   model: 'gpt-3.5-turbo',
-  //   messages: [{ role: 'system', content: 'xiexiexie ' }]
-  // })
-  // console.log('req suceess  log')
+  console.log('==>', req.body)
+
+  const message = req.body.data
+  if (!message) return res.send({ code: 400, message: '参数不能为空' })
+
+  try {
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: message }]
+    })
+    console.log(completion.data.choices[0].message?.content)
+    return res.send({ code: 200, message: completion.data.choices[0].message?.content })
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status)
+      console.log(error.response.data)
+      return res.send({
+        code: error.response.status,
+        message: error.response.data.error.message
+      })
+    } else {
+      console.log(error.message)
+    }
+  }
+  console.log('req suceess  log')
   // res.send(completion)
 })
 
@@ -61,30 +87,30 @@ app.listen(3000, () => {
 });
 
  */
-app.get('/', async (req, res) => {
-  try {
-    await axios.options('https://api.openai.com/v1/chat/completions')
-    await axios
-      .post(
-        'https://api.openai.com/v1/chat/completions',
-        {
-          messages: [{ role: 'system', content: '前端' }],
-          model: 'gpt-3.5-turbo'
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.API_KEY}`
-          }
-        }
-      )
-      .then((data) => {
-        console.log(data)
-      })
-  } catch (e) {
-    console.log('fafafafa: ')
-  }
-})
+// app.get('/', async (req, res) => {
+//   try {
+//     await axios.options('https://api.openai.com/v1/chat/completions')
+//     await axios
+//       .post(
+//         'https://api.openai.com/v1/chat/completions',
+//         {
+//           messages: [{ role: 'system', content: '前端' }],
+//           model: 'gpt-3.5-turbo'
+//         },
+//         {
+//           headers: {
+//             'Content-Type': 'application/json',
+//             Authorization: `Bearer ${process.env.API_KEY}`
+//           }
+//         }
+//       )
+//       .then((data) => {
+//         console.log(data)
+//       })
+//   } catch (e) {
+//     console.log('fafafafa: ')
+//   }
+// })
 
 app.listen(3000, () => {
   console.log(process.env.API_KEY)
